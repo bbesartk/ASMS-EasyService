a11662a6bd0260471a8161b48d3a9dba87c17a7a
Besart Kuleta <bbesart-k@msn.com>
2016-11-10 11:52:24 +0100

    dsa

--- EasyService/EasyService/Clients/Client.cs
+++ EasyService/EasyService/Clients/Client.cs
@@ -8,12 +8,12 @@ namespace EasyService.Clients
 {
     class Client:Person
     {
-        public int Id { get; set; }
-        static int _id;
+        public int Id { get; private set; }
+        static int _id = 0;
 
         public Client()
         {
-
+            Id = ++_id;
         }
         public Client(string name, string lastname, string address, string phonenumber, string email):base(name,lastname,address,phonenumber,email)
         {
