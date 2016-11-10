5d6683a299ac462d346ee2ad66281e496fdf6db5
Besart Kuleta <bbesart-k@msn.com>
2016-11-10 11:43:49 +0100

    name Changed

---
+++ EasyService/EasyService/Clients/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyService.Clients
+{
+    class Client:Person
+    {
+        public int Id { get; set; }
+        static int _id;
+
+        public Client()
+        {
+
+        }
+        public Client(string name, string lastname, string address, string phonenumber, string email):base(name,lastname,address,phonenumber,email)
+        {
+            Id = ++_id;
+        }
+
+    }
+}
