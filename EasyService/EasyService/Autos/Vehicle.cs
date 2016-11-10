d49320a233692763c76f304904a8935f2d4adbf2
Endrit Vitija <vitija.endrit@hotmail.com>
2016-11-10 13:02:18 +0100

    vehicle

---
+++ EasyService/EasyService/Autos/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyService.Autos
+{
+    class Vehicle
+    {
+        #region Properties
+        public string LicensePlate { get; set; }
+        public int VIN { get; set; }
+        public int ProductionYear { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int MotorPower { get; set; }
+        public double CurrentKm { get; set; }
+        public int MyProperty { get; set; }
+        #endregion
+    }
+}
