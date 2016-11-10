10fdcbe7276921ed4f368d45c3c84491301936f5
Besart Kuleta <bbesart-k@msn.com>
2016-11-10 13:01:30 +0100

    company

---
+++ EasyService/EasyService/Company/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyService.Company
+{
+    class Company
+    {
+        #region Properties
+        public int BusinessNumber { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public Contract Contract { get; set; }
+        #endregion
+
+        #region Methods
+        //
+        #endregion
+    }
+}
--- EasyService/EasyService/Employee/Employee.cs
+++ EasyService/EasyService/Employee/Employee.cs
@@ -9,23 +9,26 @@ namespace EasyService.Employee
     class Employee:Person
     {
         #region StaticMembers
-        static int _id = 0;
+        static int _id = 2016;
         #endregion
 
         #region Properties
-        public int Id { get; private set; }
+
+        public string Id { get; private set; }
         public bool IsMale { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; }
+
         #endregion
 
         #region Constructors
-        public Employee()
+        public Employee(string role)
         {
-            Id = ++_id;
+            Id = ++_id + role;
         }
-        public Employee(string name, string lastname, bool isMale, string address, string phonenumber, string email, string password) : base(name, lastname, address, phonenumber, email)
+        public Employee(string name, string lastname, bool isMale, string address, string phonenumber, string email, string password,string role) : base(name, lastname, address, phonenumber, email)
         {
-            Id = ++_id;
+            Id = ++_id+role;
             IsMale = isMale;
             Password = password;
         }
