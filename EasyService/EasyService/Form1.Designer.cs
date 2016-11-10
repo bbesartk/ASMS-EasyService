fe4aca52c48e6325b96cd49369dfa338e2d7eb1d
Besart Kuleta <bbesart-k@msn.com>
2016-11-10 13:47:01 +0100

    dd

--- EasyService/EasyService/Form1.Designer.cs
+++ EasyService/EasyService/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EasyService
 {
-    partial class Form1
+    partial class LoginFrm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // LoginFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "LoginFrm";
+            this.Text = "Please Identificate Yourself";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
