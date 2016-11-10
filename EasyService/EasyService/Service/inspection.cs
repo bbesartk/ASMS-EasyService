f946649832e6d403f0eca0b67d30f229c6b128bc
Endrit Vitija <vitija.endrit@hotmail.com>
2016-11-10 13:32:19 +0100

    dddd

---
+++ EasyService/EasyService/Service/inspection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyService.Service
+{
+    class Inspection
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Prise { get; set; }
+        #endregion
+
+        #region Conctrucion
+
+        public Inspection()
+        {
+
+        }
+        public Inspection(int id,string name,string description,double prise)
+        {
+
+        }
+
+        #endregion
+
+
+
+
+    }
+}
