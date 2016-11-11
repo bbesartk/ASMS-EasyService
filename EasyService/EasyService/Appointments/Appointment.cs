13371e1e8803f5c08bfd132d55c7dd4662e7d0cb
Endrit Vitija <vitija.endrit@hotmail.com>
2016-11-11 13:15:47 +0100

    appoitmant

--- EasyService/EasyService/Appointments/Appointment.cs
+++ EasyService/EasyService/Appointments/Appointment.cs
@@ -9,8 +9,24 @@ namespace EasyService.Appointments
 {
     class Appointment
     {
+        #region Properties
         public DateTime DateOfMeeting { get; set; }
         public  Vehicle Vehicle { get; set; }
         public bool IsTreated { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public Appointment()
+        {
+
+        }
+        public Appointment(DateTime dateOfMeeting,Vehicle vehicle,bool isTreated)
+        {
+            DateOfMeeting = dateOfMeeting;
+            Vehicle = vehicle;
+            IsTreated = isTreated;
+        }
+        #endregion
     }
 }
