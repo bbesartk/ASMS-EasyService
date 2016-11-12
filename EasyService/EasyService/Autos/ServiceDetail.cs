60ce0376bdb0b029a53366c66bd1e14cdfecd027
Endrit Vitija <vitija.endrit@hotmail.com>
2016-11-12 15:15:32 +0100

    service detail

--- EasyService/EasyService/Autos/ServiceDetail.cs
+++ EasyService/EasyService/Autos/ServiceDetail.cs
@@ -8,6 +8,7 @@ namespace EasyService.Autos
 {
     public class ServiceDetail
     {
+        #region Properties
         public string Description { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
@@ -18,5 +19,19 @@ namespace EasyService.Autos
                 return Quantity * Price;
             }
         }
+        #endregion
+
+        #region Constuctors
+        public ServiceDetail()
+        {
+
+        }
+        public ServiceDetail(string description,int quantity,double price)
+        {
+            Description = description;
+            Quantity = quantity;
+            Price = price;
+        }
+        #endregion
     }
 }
