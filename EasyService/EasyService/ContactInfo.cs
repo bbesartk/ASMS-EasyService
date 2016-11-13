bdfc23a9d59c2036547b165b193a0af676a6f83d
Endrit Vitija <vitija.endrit@hotmail.com>
2016-11-13 15:35:04 +0100

    endrit

--- EasyService/EasyService/ContactInfo.cs
+++ EasyService/EasyService/ContactInfo.cs
@@ -8,12 +8,60 @@ namespace EasyService
 {
     class ContactInfo
     {
-        public string City { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
+        #region PrivateMembers
+        private string _city;
+        private string _address;
+        private string _phoneNumber;
+        private string _email;
+        #endregion
+
+        #region Properties
+        public string City { get { return _city; }
+            set
+            {
+                if (_city != String.Empty)
+                {
+                    _city = value;
+                }
+                else
+                {
+                    throw new Exception("Shkruani Qytetin");
+                }
+            }
+        }
+        public string Address { get { return _address; }
+            set
+            {
+                if (_address != String.Empty)
+                {
+                    _address = value;
+                }
+                else
+                {
+                    throw new Exception("Shkruani addressen");
+                }
+            }
+        }
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                if (_phoneNumber != String.Empty)
+                {
+                    _phoneNumber = value;
+                }
+                else
+                {
+                    throw new Exception("Shkruani numrin e telefonit");
+                }
+            }
         public string Email { get; set; }
+        
+           
+        #endregion
 
-
+            #region Constructors
         public ContactInfo()
         {
 
@@ -26,5 +74,6 @@ namespace EasyService
             PhoneNumber = phoneNumber;
             Email = email;
         }
+        #endregion
     }
 }
