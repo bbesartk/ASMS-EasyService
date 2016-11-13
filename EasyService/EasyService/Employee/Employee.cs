62a57069f6158478d1d98d8405e71381a8d7ea5a
Endrit Vitija <vitija.endrit@hotmail.com>
2016-11-13 15:18:15 +0100

    dkdkkd

--- EasyService/EasyService/Employee/Employee.cs
+++ EasyService/EasyService/Employee/Employee.cs
@@ -6,18 +6,66 @@ using System.Threading.Tasks;
 
 namespace EasyService.Employee
 {
-    class Employee:Person
+    class Employee : Person
     {
         #region StaticMembers
         static int _id = DateTime.Now.Year;
         #endregion
 
+        #region PrivateMembers
+        private string _password;
+        private ContactInfo _contactInfo;
+        private string _role;
+        #endregion
+
         #region Properties
 
         public string Id { get; private set; }
-        public string Password { get; set; }
-        public ContactInfo ContactInfo { get; set; }
-        public string Role { get; set; }
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (_password.Length > 4)
+                {
+                    _password = value;
+                }
+                else
+                {
+                    throw new Exception("Gjasia e Passwordit duhet te jet mbi 4 karaktere");
+                }
+            }
+        }
+        public ContactInfo ContactInfo
+        {
+            get { return _contactInfo; }
+            set
+            {
+                if (_contactInfo != null)
+                {
+                    _contactInfo = value;
+                }
+                else
+                {
+                    throw new Exception("Kontakt info nuk duhet te jet i zbrazet");
+                }
+            }
+        }
+
+
+        public string Role { get { return _role; }
+            set
+            {
+                if (_role != String.Empty)
+                {
+                    _role = value;
+                }
+                else
+                {
+                    throw new Exception("Roli nuk guxon te jet i zbrazet");
+                }
+            }
+        }
 
         #endregion
 
@@ -26,9 +74,9 @@ namespace EasyService.Employee
         {
             Id = ++_id + role;
         }
-        public Employee(string name, string lastname, bool isMale,ContactInfo contactInfo, string password,string role) : base(name, lastname, isMale)
+        public Employee(string name, string lastname, bool isMale, ContactInfo contactInfo, string password, string role) : base(name, lastname, isMale)
         {
-            Id = ++_id+role;
+            Id = ++_id + role;
             IsMale = isMale;
             ContactInfo = contactInfo;
             Password = password;
