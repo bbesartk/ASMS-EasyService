45ee57b613a2cd672b0dfc64d9a0699702badb68
Endrit Vitija <Endrit Vitija@DESKTOP-GI5UP23>
2016-12-16 20:22:39 +0100

    uc_RegisterServiceName

---
+++ EasyService/EasyService.UI/UC_RegisterServiceName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyService.UI
+{
+    public partial class UC_RegisterServiceName : UserControl
+    {
+        public UC_RegisterServiceName()
+        {
+            InitializeComponent();
+        }
+
+        private void UC_RegisterServiceName_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
