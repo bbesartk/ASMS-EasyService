9ff178376080cfd0144c18242e31176b50a033a4
Besart Kuleta <bbesart-k@msn.com>
2016-12-19 11:47:36 +0100

    new

--- EasyService/ES.BusinessLayer/blAppointments.cs
+++ EasyService/ES.BusinessLayer/blAppointments.cs
@@ -12,34 +12,10 @@ namespace ES.BusinessLayer
     public class blAppointments
     {
         #region CRUD
-        //public static void InsertAppointment(Appointment appointment)
-        //{
-        //    if (appointment != null)
-        //        dalAppointments.Insert(appointment);
-        //    else throw new Exception("Invalid Appointment");
-        //}
-        //public static void UpdateAppointment(Appointment appointment)
-        //{
-        //    if (appointment != null)
-        //        dalAppointments.Update(appointment);
-        //    else throw new Exception("Invalid Appointment");
-        //}
 
-        //public static void DeleteAppointment(string id)
-        //{
-        //    if (id != "")
-        //        dalAppointments.Remove(id);
-        //    else throw new Exception("Invalid Id!");
-        //}
-        //public static Appointment GetAppointment(string id)
-        //{
-        //    return dalAppointments.GetAppointment(id);
-        //}
+        #endregion
 
-
-        //#endregion
-
-        //#region Query
+        #region Query
         public static List<Appointment> GetAllAppointment()
         {
             return dalAppointments.GetAll();
